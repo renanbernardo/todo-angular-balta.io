695daa7ca59b16f9d0d27031852141589fd1e546
renanbernardo <renan.brnrd@gmail.com>
2021-05-10 17:22:05 -0300

    Commit Inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  public todos: Todo[] = []; // []
+  // public todos: any[]; // undefined
+  public title: String = 'Minhas Tarefas';
+
+  // ctor
+  constructor() {
+    // this. ele se refere ao escopo da classe
+    this.todos.push(new Todo(1, 'Tarefa 1', false));
+    this.todos.push(new Todo(1, 'Tarefa 1', false));
+    this.todos.push(new Todo(1, 'Tarefa 1', true));
+  }
 }
