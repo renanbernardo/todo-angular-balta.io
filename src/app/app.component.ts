3f061629f5ee29d257c705d9ef76e3aed07453b6
renanbernardo <renan.brnrd@gmail.com>
2021-05-11 08:41:57 -0300

    Forms Básico

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Todo } from 'src/models/todo.model';
 
 @Component({
@@ -10,12 +11,37 @@ export class AppComponent {
   public todos: Todo[] = []; // []
   // public todos: any[]; // undefined
   public title: String = 'Minhas Tarefas';
+  public form: FormGroup;
 
   // ctor
-  constructor() {
-    // this. ele se refere ao escopo da classe
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required
+      ])]
+    })
+    // this. se refere ao escopo da classe
     this.todos.push(new Todo(1, 'Tarefa 1', false));
-    this.todos.push(new Todo(1, 'Tarefa 1', false));
-    this.todos.push(new Todo(1, 'Tarefa 1', true));
+    this.todos.push(new Todo(2, 'Tarefa 2', false));
+    this.todos.push(new Todo(3, 'Tarefa 3', true));
+    this.todos.push(new Todo(4, 'Tarefa 4', false));
   }
+
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo);    
+    if (index !== -1) {
+      this.todos.splice(index, 1);
+    }
+  }
+
+  markAsDone(todo: Todo) {
+    todo.done = true;
+  }
+
+  markAsUndone(todo: Todo) {
+    todo.done = false;
+  }
+
 }
