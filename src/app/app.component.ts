18bda10487bd39df883ab298538ec2c7241755fd
renanbernardo <renan.brnrd@gmail.com>
2021-05-11 14:39:49 -0300

    Estiliza os itens da lista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Todo } from 'src/models/todo.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public mode = 'list';
   public todos: Todo[] = []; // []
   // public todos: any[]; // undefined
   public title: String = 'Minhas Tarefas';
@@ -61,6 +62,7 @@ export class AppComponent {
   save() {
     const data = JSON.stringify(this.todos);
     localStorage.setItem('todos', data);
+    this.mode = 'list';
   }
 
   load() {
@@ -70,5 +72,9 @@ export class AppComponent {
     } else {
       this.todos = []
     }
+  }
+
+  changeMode(mode: string) {
+    this.mode = mode;
   }
 }
